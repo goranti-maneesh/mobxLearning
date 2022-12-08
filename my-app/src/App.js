cf320c1a07a61bd90000701efff90a2bee9b977d
goranti-maneesh <gorantimaneeshreddy@gmail.com>
2022-12-08 20:46:32 +0530

    message

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,49 +1,50 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-import {useObserver} from "mobx-react"
+import { useObserver } from "mobx-react";
 
-import {useContextStore} from './Context/context'
+import { useContextStore } from "./Context/context";
 
 const App = () => {
+	const [text, setText] = useState("");
 
-  const [text, setText] = useState('')
+	const contextStore = useContextStore();
 
-  const contextStore = useContextStore()
-  
-  const onChangeText = (event) => {
-    setText(event.target.value)
-  }
+	const onChangeText = (event) => {
+		setText(event.target.value);
+	};
 
-  const onClickAddNote = () => {
-    contextStore.addNote(text)
-    setText('')
-    console.log(contextStore, "contextStore")
-  }
+	const onClickAddNote = () => {
+		contextStore.addNote(text);
+		setText("");
+	};
 
-  const onClickDeleteNote = (id) => {
-    contextStore.deleteNote(id)
-  }
+	const onClickDeleteNote = (id) => {
+		contextStore.deleteNote(id);
+	};
 
-  return(
-    useObserver(() => {
-  return (
-    <div className="App">
-      <input type="text" value={text} onChange={onChangeText} />
-      <button onClick={onClickAddNote}>Add Note</button>
-      <ul>
-        {contextStore.notesList.map((eachNote) => {
-          <li key={eachNote.id}>
-            <p>{eachNote.note}</p>
-            <button onClick={() => onClickDeleteNote(eachNote.id)}>
-              delete
-            </button>
-          </li>
-        })}
-      </ul>
-    </div>
-  )
-      }))  
-}
+	return useObserver(() => {
+		return (
+			<div className="App">
+				<input type="text" value={text} onChange={onChangeText} />
+				<button onClick={onClickAddNote}>Add Note</button>
+
+				<ul>
+					{contextStore.notesList.map((eachNote) => (
+            
+            <li key={eachNote.id}>
+							<p>{eachNote.note}</p>
+							<button
+								onClick={() => onClickDeleteNote(eachNote.id)}>
+								delete
+							</button>
+						</li>
+
+					))}
+				</ul>
+			</div>
+		);
+	});
+};
 
 export default App;
--- my-app/src/Stores/store.js
+++ my-app/src/Stores/store.js
@@ -4,9 +4,7 @@ export const createStore = () => {
     return{
         notesList: [],
         addNote(note){
-            console.log(note)
             this.notesList.push({note, id: nanoid()})
-            console.log(this.notesList)
         },
         deleteNote(id) {
             const updatedNotesList = this.notesList.filter((eachNote) => eachNote.id !== id)
